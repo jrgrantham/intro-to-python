e00fa5e489b290c5b15a1fb4d0cdac9e6eded00a
James Grantham <james.r.grantham@gmail.com>
2020-02-10 15:41:44 +0000

    comprehension

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -11,6 +11,8 @@ for more info regarding list comprehensions.
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
 y = []
+for x in range(5):
+  y.append( x + 1 )
 
 print (y)
 
@@ -18,6 +20,8 @@ print (y)
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
 y = []
+for x in range(10):
+  y.append( x*x*x )
 
 print(y)
 
@@ -27,6 +31,8 @@ print(y)
 a = ["foo", "bar", "baz"]
 
 y = []
+for word in a:
+  y.append(word.upper())
 
 print(y)
 
@@ -35,7 +41,16 @@ print(y)
 
 x = input("Enter comma-separated numbers: ").split(',')
 
+print(x)
+
 # What do you need between the square brackets to make it work?
+y = [int(number) for number in x if int(number) % 2 == 0]
+
+print(y)
+
 y = []
+for number in x:
+  if int(number) % 2 == 0:
+    y.append(int(number))
 
 print(y)
